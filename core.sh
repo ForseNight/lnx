45e44455ac2bfc9e1a3c0374374c0c5cf8697fe1
ForseNight <110750123+ForseNight@users.noreply.github.com>
2025-12-09 14:08:03 +0300

    Update core.sh

--- core.sh
+++ core.sh
@@ -1,43 +1,38 @@
+#!/bin/bash
+set -e
+
 DISK="/dev/sda"
+EFI="${DISK}1"
+ROOT="${DISK}2"
 
-parted -s $DISK mklabel gpt
-parted -s $DISK mkpart ESP fat32 1MiB 301MiB
-parted -s $DISK set 1 esp on
-parted -s $DISK mkpart primary ext4 301MiB 100%
+cfdisk "$DISK"
 
-mkfs.fat -F32 ${DISK}1
-mkfs.ext4 -F ${DISK}2
+mkfs.fat -F32 "$EFI"
+mkfs.ext4 "$ROOT"
 
-mount ${DISK}2 /mnt
-mkdir /mnt/boot
-mount ${DISK}1 /mnt/boot
+mount "$ROOT" /mnt
+mkdir -p /mnt/boot
+mount "$EFI" /mnt/boot
 
-pacstrap /mnt base linux linux-firmware networkmanager plasma-desktop plasma-meta kde-system-meta sddm
+pacstrap /mnt base linux linux-firmware networkmanager plasma sddm
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
-cat << EOF > /mnt/chroot.sh
+arch-chroot /mnt /bin/bash -c "
+set -e
 ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 hwclock --systohc
-sed -i 's/#ru_RU.UTF-8 UTF-8/ru_RU.UTF-8 UTF-8/' /etc/locale.gen
-sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
+sed -i 's/#ru_RU.UTF-8/ru_RU.UTF-8/' /etc/locale.gen
 locale-gen
-echo LANG=ru_RU.UTF-8 > /etc/locale.conf
-echo arch > /etc/hostname
-cat << H > /etc/hosts
-127.0.0.1 localhost
-::1 localhost
-127.0.1.1 arch
-H
+echo 'LANG=ru_RU.UTF-8' > /etc/locale.conf
+echo 'arch' > /etc/hostname
 systemctl enable NetworkManager
 systemctl enable sddm
 pacman -S --noconfirm grub efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
-echo root:123 | chpasswd
-EOF
+passwd
+"
 
-chmod +x /mnt/chroot.sh
-arch-chroot /mnt /chroot.sh
-rm /mnt/chroot.sh
 reboot
