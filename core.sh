84031bd400111f9ad17275703c9f6fe032c0c0a6
ForseNight <110750123+ForseNight@users.noreply.github.com>
2025-12-09 13:58:30 +0300

    Update core.sh

--- core.sh
+++ core.sh
@@ -1,93 +1,43 @@
-#!/bin/bash
-
-### ===== НАСТРОЙКИ =====
 DISK="/dev/sda"
-HOSTNAME="arch"
-LOCALE="ru_RU.UTF-8"
-KEYMAP="ru"
-TIMEZONE="Europe/Moscow"
 
-### ===== ПРОВЕРКА =====
-echo "!!! ВНИМАНИЕ: СЕЙЧАС БУДЕТ СТЁРТ ДИСК $DISK !!!"
-read -p "Нажми Enter чтобы продолжить..."
-
-### ===== РАЗМЕТКА ДИСКА =====
-echo "[+] Создание GPT и разделов..."
 parted -s $DISK mklabel gpt
 parted -s $DISK mkpart ESP fat32 1MiB 301MiB
 parted -s $DISK set 1 esp on
 parted -s $DISK mkpart primary ext4 301MiB 100%
 
-EFI="${DISK}1"
-ROOT="${DISK}2"
+mkfs.fat -F32 ${DISK}1
+mkfs.ext4 -F ${DISK}2
 
-### ===== ФОРМАТИРОВАНИЕ =====
-echo "[+] Форматирование..."
-mkfs.fat -F32 $EFI
-mkfs.ext4 -F $ROOT
-
-### ===== МОНТИРОВАНИЕ =====
-echo "[+] Монтирование..."
-mount $ROOT /mnt
+mount ${DISK}2 /mnt
 mkdir /mnt/boot
-mount $EFI /mnt/boot
+mount ${DISK}1 /mnt/boot
 
-### ===== УСТАНОВКА СИСТЕМЫ =====
-echo "[+] Установка Arch Linux + KDE..."
-pacstrap /mnt base linux linux-firmware nano networkmanager \
-    plasma sddm konsole
+pacstrap /mnt base linux linux-firmware networkmanager plasma-desktop plasma-meta kde-system-meta sddm
 
-### ===== FSTAB =====
 genfstab -U /mnt >> /mnt/etc/fstab
 
-### ===== CHROOT-СКРИПТ =====
-cat << EOF > /mnt/chroot-setup.sh
-#!/bin/bash
-
-echo "[CHROOT] Настройка системы..."
-
-# Часовой пояс
-ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+cat << EOF > /mnt/chroot.sh
+ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 hwclock --systohc
-
-# Локали
-sed -i "s/#$LOCALE UTF-8/$LOCALE UTF-8/" /etc/locale.gen
-sed -i "s/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/" /etc/locale.gen
+sed -i 's/#ru_RU.UTF-8 UTF-8/ru_RU.UTF-8 UTF-8/' /etc/locale.gen
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
-echo "LANG=$LOCALE" > /etc/locale.conf
-
-# hostname
-echo "$HOSTNAME" > /etc/hostname
-
-# hosts
-cat << HOSTS > /etc/hosts
-127.0.0.1    localhost
-::1          localhost
-127.0.1.1    $HOSTNAME
-HOSTS
-
-# Сеть
+echo LANG=ru_RU.UTF-8 > /etc/locale.conf
+echo arch > /etc/hostname
+cat << H > /etc/hosts
+127.0.0.1 localhost
+::1 localhost
+127.0.1.1 arch
+H
 systemctl enable NetworkManager
-
-# SDDM
 systemctl enable sddm
-
-# Установка grub
 pacman -S --noconfirm grub efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
-
-echo "[CHROOT] Установи пароль root:"
-passwd
-
+echo root:123 | chpasswd
 EOF
 
-chmod +x /mnt/chroot-setup.sh
-
-### ===== ЗАПУСК CHROOT СКРИПТА =====
-arch-chroot /mnt /chroot-setup.sh
-rm /mnt/chroot-setup.sh
-
-echo "[+] Установка завершена! Перезагружаюсь..."
-sleep 3
+chmod +x /mnt/chroot.sh
+arch-chroot /mnt /chroot.sh
+rm /mnt/chroot.sh
 reboot
