51c00a4ac99be563a4ac1689dc54cb918a96845f
ForseNight <110750123+ForseNight@users.noreply.github.com>
2025-12-09 14:26:34 +0300

    core.sh

--- core.sh
+++ core.sh
@@ -1,38 +1,20 @@
-#!/bin/bash
-set -e
-
-DISK="/dev/sda"
-EFI="${DISK}1"
-ROOT="${DISK}2"
-
-cfdisk "$DISK"
-
-mkfs.fat -F32 "$EFI"
-mkfs.ext4 "$ROOT"
-
-mount "$ROOT" /mnt
-mkdir -p /mnt/boot
-mount "$EFI" /mnt/boot
-
-pacstrap /mnt base linux linux-firmware networkmanager plasma sddm
-
+sgdisk -Z /dev/sda
+sgdisk -n 1:0:+512M -t 1:EF00 /dev/sda   # EFI
+sgdisk -n 2:0:0 -t 2:8300 /dev/sda      # /
+mkfs.fat -F32 /dev/sda1
+mkfs.ext4 /dev/sda2
+mount /dev/sda2 /mnt
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
+pacstrap /mnt base linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
-
-arch-chroot /mnt /bin/bash -c "
-set -e
-ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
-hwclock --systohc
-sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
-sed -i 's/#ru_RU.UTF-8/ru_RU.UTF-8/' /etc/locale.gen
+arch-chroot /mnt /bin/bash <<EOF
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
-echo 'LANG=ru_RU.UTF-8' > /etc/locale.conf
-echo 'arch' > /etc/hostname
-systemctl enable NetworkManager
-systemctl enable sddm
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "archlinux" > /etc/hostname
+echo "root:password" | chpasswd
 pacman -S --noconfirm grub efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
-passwd
-"
-
-reboot
+EOF
